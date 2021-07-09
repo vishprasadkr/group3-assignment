e589676c7cd683358c6091c4b54f60d330a8e85c
Cloud User <auto@automation.o1wnlwy3bobebl2oeyj41pd2zh.ux.internal.cloudapp.net>
2021-07-09 06:41:21 +0000

    Outputs changed

--- modules/common/output.tf
+++ modules/common/output.tf
@@ -1,9 +1,9 @@
 output "log_analytics_workspace" {
-  value = azurerm_log_analytics_workspace.log-ana-work
+  value = azurerm_log_analytics_workspace.log-ana-work.name
 }
 output "storage_account" {
-  value = azurerm_storage_account.storage_acc
+  value = azurerm_storage_account.storage_acc.name
 }
 output "storage_container" {
-  value = azurerm_storage_container.storage_container
+  value = azurerm_storage_container.storage_container.name
 }
--- output.tf
+++ output.tf
@@ -8,10 +8,7 @@ output "private_ips_linux" {
 output "public_ips_linux" {
   value = module.linux.linux_public_ip
 }
-# output "linux_nic" {
-#   # value = [azurerm_public_ip.linux_pub_ip[*].ip_address]
-#   value = module.linux.linux_nic
-# }
+
 # Windows output
 output "vm_hostname_main_windows" {
   value = module.vmwindows.windows_hostname
@@ -25,4 +22,29 @@ output "public_ips_windows" {
 
 output "resource_group_name" {
   value = module.resource_group.rg_name
+}
+
+# common output
+output "Log-Workspace" {
+  value = module.common.log_analytics_workspace
+}
+
+output "Storage_Account" {
+  value = module.common.storage_account
+}
+
+output "storage_container" {
+  value = module.common.storage_container
+}
+
+# network
+output "vnet" {
+    value = module.network.vnet  
+}
+output "Subnet1" {
+    value = module.network.subnet1
+}
+# database
+output "post_server" {
+  value= module.database.post_server
 }
